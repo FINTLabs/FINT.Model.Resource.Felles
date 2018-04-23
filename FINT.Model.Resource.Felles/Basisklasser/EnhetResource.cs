dbbd171f567a719a5005dd3786601b5c3144de5f
Frode Sjovatsen <frode@fintprosjektet.no>
2018-04-23 15:34:41 +0200

    fixed public prop in enhet

--- FINT.Model.Resource.Felles/Basisklasser/EnhetResource.cs
+++ FINT.Model.Resource.Felles/Basisklasser/EnhetResource.cs
@@ -15,7 +15,7 @@ namespace FINT.Model.Felles.Basisklasser
         }
 
         [JsonProperty(PropertyName = "_links")]
-        public Dictionary<string, List<Link>> Links { get; private set; }
+        public new Dictionary<string, List<Link>> Links { get; private set; }
 
         public AdresseResource Forretningsadresse { get; set; }
         public string Organisasjonsnavn { get; set; }
